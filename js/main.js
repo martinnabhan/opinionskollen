ca9ab4fd37974cdcaec321fc4bb94a1fb050e996
MartinN13 <martinnabhan@me.com>
2016-05-16 00:35:53 +0200

    Refractor hamburger menu code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 /* disable scrolling */
 function toggleScroll() {
-  console.log("Stopping scrolling");
-
   $('body').on('mousewheel', function(e) {
     if ($('body').hasClass('noscroll')) {
       e.preventDefault();
@@ -10,18 +8,23 @@ function toggleScroll() {
 }
 
 /* hamburger menu animation */
-$(document).ready(function() {
+function toggleNav() {
   toggleScroll();
-  $(".hamburger").click(function() {
-    $(".nav").fadeToggle(400);
-    $("body").toggleClass("noscroll");
-  });
+  $(".hamburger").toggleClass("is-active");
+  $(".nav").fadeToggle(400);
+  $("body").toggleClass("noscroll");
+}
+
+$(document).ready(function() {
+  /* hamburger menu trigger */
+  $(".hamburger").click(toggleNav);
 });
 
 /* make esc exit hamburger menu */
 $(document).keydown(function(e) {
   if (e.keyCode == 27) {
-    $(".nav").fadeOut(500);
-    $("body").removeClass("noscroll");
+    if ($(".hamburger").hasClass("is-active")) {
+      toggleNav();
+    }
   }
 });
